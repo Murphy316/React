7b719be9be73de9e47f3324c7fb1191e2291f26a
Murphy316 <liqi316@live.com>
2020-05-22 11:13:53 -0700

    react hello world

---
+++ src/App.js
@@ -0,0 +1,26 @@
+/*
+ * @Author: Murphy
+ * @Date: 2020-05-22 10:55:14
+ * @LastEditTime: 2020-05-22 10:58:15
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \React Hello World\hello-world\src\App.js
+ */ 
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hello World
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
